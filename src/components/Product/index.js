b0b9b108f2481e51aa23507b2f1e4ebb18eba597
gustavo moura <g.gustavosouma@gmail.com>
2022-02-12 17:42:35 -0300

    finalizado

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,25 +1,26 @@
-import {useState} from 'react'
-import './style.css'
+import { useState } from "react";
+import "./style.css";
 
-function Product({name, category, price, cart, setCart, img}) {
-
+function Product({ name, produtos, id, category, price, cart, setCart, img }) {
   const [item, setItem] = useState({
     name: name,
-    category : category,
-    price : price,
-    quantidade : 0,
-    img : img
-  })
+    category: category,
+    price: price,
+    quantidade: 0,
+    img: img,
+  });
 
- function Adicionar(){
-   if(item.quantidade === 0){
-     setCart([...cart, item])
-     item.quantidade++
-  }else {
-    item.quantidade++
-    setCart([...cart])
+  function Adicionar(){
+    const produtoEncontrado = produtos.find((produto) => produto.id == id)
+    const produtoNoCarrinho = cart.find((produtinho) => produtinho.id === produtoEncontrado.id)
+    if(produtoNoCarrinho){
+      produtoNoCarrinho.quantidade++;
+      setCart([...cart])
+    }else {
+      produtoEncontrado.quantidade = 1
+      setCart([...cart, produtoEncontrado])
+    }
   }
- }
 
   return (
     <div className="produto">
@@ -35,7 +36,7 @@ function Product({name, category, price, cart, setCart, img}) {
         </button>
       </div>
     </div>
-  );       
+  );
 }
 
 export default Product;
--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,20 +1,38 @@
-import Product from "../Product"
-import './style.css'
+import Product from "../Product";
+import "./style.css";
 
-function ProductList({salvarProdutos,produtos, cart, setCart}){
-    return(
-        <div className="container">  
-            {salvarProdutos.length > 0 ? 
-                salvarProdutos?.map((produto) => 
-                <Product key={produto.id} name={produto.name} category ={produto.category} price={produto.price} cart={cart} setCart={setCart} img={produto.img}/>  
-                )
-                :( 
-                produtos?.map((produto) => 
-                <Product key={produto.id} name={produto.name} category ={produto.category} price={produto.price} cart={cart} setCart={setCart} img={produto.img}/>  
-                )
-            )}     
-        </div>
-    )
+function ProductList({ salvarProdutos, produtos, cart, setCart }) {
+  return (
+    <div className="container">
+      {salvarProdutos.length > 0
+        ? salvarProdutos?.map((produto) => (
+            <Product
+              produtos={produtos}
+              key={produto.id}
+              id={produto.id}
+              name={produto.name}
+              category={produto.category}
+              price={produto.price}
+              cart={cart}
+              setCart={setCart}
+              img={produto.img}
+            />
+          ))
+        : produtos?.map((produto) => (
+            <Product
+              produtos={produtos}
+              key={produto.id}
+              id={produto.id}
+              name={produto.name}
+              category={produto.category}
+              price={produto.price}
+              cart={cart}
+              setCart={setCart}
+              img={produto.img}
+            />
+          ))}
+    </div>
+  );
 }
 
-export default ProductList
+export default ProductList;
