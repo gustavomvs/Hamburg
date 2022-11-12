a2cd122a0833e6ed5d8ecf130192b106bd94e63a
gustavomvs <g.gustavosouma@gmail.com>
2022-11-12 18:08:26 -0300

    Hamb Project

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,38 +1,42 @@
-import imagem from '../../image/Mask Group.svg'
-import './style.css'
-import {useState} from 'react'
+import imagem from "../../image/Mask Group.svg";
+import "./style.css";
+import { useState } from "react";
 
-function Header({produtos, setSalvarProdutos}){
-
+function Header({ produtos, setSalvarProdutos }) {
   const [busca, setBusca] = useState("");
 
-  function filtro(){
-    const produtoAchado = produtos.filter((produto) => {
-        if(produto.category.toLowerCase() === busca.toLowerCase()){
-            return produto
-        } 
-        if (produto.name.toLowerCase() === busca.toLowerCase()){
-          return produto;
-        }
-    })
-    if(produtoAchado){
-        setSalvarProdutos(produtoAchado)
-    }else{
-        setSalvarProdutos([])
+  function filtro() {
+    const vitrineFiltrada = produtos.filter((item) => {
+      return (
+        item.name.toLocaleLowerCase().includes(busca.toLocaleLowerCase()) ||
+        item.category.toLocaleLowerCase().includes(busca.toLocaleLowerCase())
+      );
+    });
+
+    if (vitrineFiltrada) {
+      setSalvarProdutos(vitrineFiltrada);
+    } else {
+      setSalvarProdutos([]);
     }
-}
+  }
 
-    return (
-        <div>
-            <div className='header'>
-                <img className='img' src={imagem}/>
-                <div className='busca'>
-                <input className='pesquisa' placeholder='Digite Pesquisa' onChange={(e) => setBusca(e.target.value)}/>
-                <button className='botao' onClick={filtro}>Pesquisar</button>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="header">
+        <img className="img" alt="imagem" src={imagem} />
+        <div className="busca">
+          <input
+            className="pesquisa"
+            placeholder="Digite Pesquisa"
+            onChange={(e) => setBusca(e.target.value)}
+          />
+          <button className="botao" onClick={filtro}>
+            Pesquisar
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
